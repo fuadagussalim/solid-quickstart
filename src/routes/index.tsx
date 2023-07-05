322d348202aa3b150a11ae08f3bb28f1af27faf7
gixpatial <fagussalim518@gmail.com>
2023-07-05 22:16:29 +0700

    wait

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,11 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
+      <Title>WebGIS  Kepanewon Ngaglik</Title>
+      <h1>Masih Proses Gaes</h1>
+      
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        Ditunggu..
       </p>
     </main>
   );
