43ddd411b17aaa9c8d6f05c6e276ac0960b08e9c
gixpatial <fagussalim518@gmail.com>
2023-07-07 01:46:33 +0700

    menambahkan navbar, hero, dan section

--- src/root.tsx
+++ src/root.tsx
@@ -26,11 +26,11 @@ function App() {
         
         <link rel="preconnect" href="https://fonts.gstatic.com" />
 
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Open+Sans&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Open+Sans&display=swap" rel="preconnect"/>
                         
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="preconnect"/>
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" rel="stylesheet"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" rel="preconnect"/>
 
         <link href="css/templatemo-topic-listing.css" rel="stylesheet"/>  
 
